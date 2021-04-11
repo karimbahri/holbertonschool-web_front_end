d7d4384aa5f4b263776a144a6881e6f34c936e42
karim bahri <karim-bahri-18@outlook.fr>
2021-04-11 02:15:10 +0100

    2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+'use strict';
+const welcomeMessage = function(fullName) {
+    alert(`Welcome ${fullName}`);
+}
+const guillaume = welcomeMessage('Guillaume');
+const alex = welcomeMessage('Alex');
+const fred = welcomeMessage('Fred');
