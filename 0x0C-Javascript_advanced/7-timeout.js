75fc41c0ad7e4ee6fcc735c50bc48c16a0536c98
karim bahri <karim-bahri-18@outlook.fr>
2021-04-11 23:07:22 +0100

    7

---
+++ 0x0C-Javascript_advanced/7-timeout.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log('Start of the execution queue');
+setTimeout(function() {
+    console.log('Final code block to be executed')
+}, 0);
+for (let i = 0; i < 100; i++) {
+    console.log(i + 1);
+}
+console.log('End of the loop printing');
