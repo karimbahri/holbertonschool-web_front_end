62b1bb58c026f68cba8c74821e20fea793993cd3
karim bahri <karim-bahri-18@outlook.fr>
2021-04-12 01:52:07 +0100

    13

---
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -0,0 +1,15 @@
+'use strict';
+const user = {
+    hobby: 'Calligraphy',
+    favoriteSport: 'Hockey',
+    astrologicalSign: 'Aries',
+    firstName: 'Buillaume',
+    lastName: 'Ialva',
+    location: 'Telaviv',
+    occupation: 'Engineer',
+}
+const logWelcomeUser = welcomeString => {
+    console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`);
+}
+const bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser('Welcome');
