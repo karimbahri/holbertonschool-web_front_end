8c1b9f560dfbd67adee1eef524adc453c0a30bae
karim bahri <karim-bahri-18@outlook.fr>
2021-04-12 01:25:40 +0100

    11

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,21 @@
+'use strict';
+const countPrimeNumbers = function() {
+    let len = 0;
+    for (let i = 2 ; i < 101 ; i++) {
+        for (let j = 0; j < i; j++) {
+            if (!(i % j)) {
+                len++;
+            }
+        }
+    }
+    return (len);
+}
+const beg = performance.now();
+setTimeout(()=>{
+    let len;
+    for (let i = 0 ; i < 100; i++)
+        len = countPrimeNumbers();
+}, 0);
+
+const end = performance.now();
+console.log(`time of calculating prime numbers 100 times was ${end - beg} milliseconds.`);
